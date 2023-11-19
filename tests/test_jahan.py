9b81c08bbf5fbcd6ca6d0f0a36c3638fba8842ad
faishalmanzar <faishalmanzar@gmail.com>
2023-11-19 21:56:11 +0530

    Prepared for publishing at pypi

--- tests/test_jahan.py
+++ tests/test_jahan.py
@@ -5,9 +5,9 @@ import unittest
 # Add the parent directory of the current file to sys.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
+sys.path.insert(0, parent_dir) 
 
-from jahan import Jahan, Request, Response, TemplateResponse
+from src.jahan.jahan import Jahan, Request, Response, TemplateResponse
 
 
 class TestJahan(unittest.TestCase):
--- tests/test_server.py
+++ tests/test_server.py
@@ -8,7 +8,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
-from server import parse_header
+from src.server.server import parse_header
 
 
 class TestParsingFunctions(unittest.TestCase):
